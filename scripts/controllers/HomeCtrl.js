72c2333d30d02035c46649fe467e3a6b0cc55e3a
Grant <gsbackes@gmail.com>
2017-07-29 11:21:56 +0900

    Checkpoint 4: chatroom name changes on click

--- scripts/controllers/HomeCtrl.js
+++ scripts/controllers/HomeCtrl.js
@@ -2,29 +2,14 @@
 	function HomeCtrl(Room, $uibModal, $log, $document) {
 		var home = this;
 		home.room = Room;
-		// home.activeRoom = 
-		// home.rooms = Room.all;
+		home.activeRoom = 'room1';
 
 
 		home.openModal = function() {
-
 			var modalInstance = $uibModal.open({
 				templateUrl: '/templates/modalContent.html',
 				controller: 'ModalCtrl as modal'
-				// How does resolve work???
-				// resolve: {
-				// 	newRoom: function() {
-				// 		return home.newRoom;
-				// 	}
-				// }	
 			});
-
-			// Don't understand this
-			// modalInstance.result.then(function (enteredRoomName) {
-			//   home.newRoom = enteredRoomName;
-			// }, function () {
-			//   $log.info('Modal dismissed at: ' + new Date());
-			// });
 		}
 	}
 
--- scripts/controllers/ModalCtrl.js
+++ scripts/controllers/ModalCtrl.js
@@ -1,19 +1,12 @@
 (function() {
 	function ModalCtrl($uibModalInstance, Room) {
 		var modal = this;
-		// $scope.newRoom = newRoom;
 
 		// Accept the room entered in the input bar
 		modal.acceptRoom = function(roomName) {
 			Room.addRoom(roomName);
-			// What about the return value here???
 			$uibModalInstance.close();
 		};
-		
-		// modal.accept = function() {
-		// 	// What about the return value here???
-		// 	// $uibModalInstance.close(newRoom);
-		// };
 
 		// Exit the modal window
 		modal.cancel = function() {
